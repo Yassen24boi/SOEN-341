85cb3fe12d74e12c30ff46c537215075664eeb37
2273957 <123512312+2273957@users.noreply.github.com>
2026-09-24 18:51:00 -0400

    Add Login and Profile components with rrouting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,18 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './Login'
+import Profile from './Profile'
 import './App.css'
 
 function App() {
-
-  const [fullName, setFullName] = useState('')
-  const [email, setEmail] = useState('')
-  const [headline, setHeadline] = useState('')
-  const [resumeFile, setResumeFile] = useState(null)
-
   return (
-    <div>
-    <h1>My Profile</h1>
-
-    <label> Full Name: </label>
-    <input
-     type ="text"
-     value={fullName}
-     onChange={() => setFullName(e.target.value)}
-     />
-
-    <label>  Email: </label>
-    <input
-     type ="email"
-     value={email}
-     onChange={() => setEmail(e.target.value)}
-     />
-
-    <label>  Headline: </label>
-    <input
-     type ="text"
-     value={headline}
-     onChange={() => setHeadline(e.target.value)}
-     />
-
-    <label> Resume: </label>
-    <input
-    type = "file"
-    accept= ".pdf, .doc, .docx"
-    onChange={(e) => setResumeFile(e.target.files[0])}
-    />
-
-    {resumeFile && <p>Selected file: {resumeFile.name}</p>}
-    <br />
-    <button onClick={()=> alert('profile saved (for now, just local - no backend yet)!')}>
-      save Profile
-      </button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/create-account" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
